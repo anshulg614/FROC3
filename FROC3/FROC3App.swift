ae3d2c4b4f944ead4d51ce870dce2acc18409fa6
anshulg614 <122504590+anshulg614@users.noreply.github.com>
2024-03-22 16:53:17 -0400

    Initial Commit

---
+++ FROC3/FROC3App.swift
@@ -0,0 +1,17 @@
+//
+//  FROC3App.swift
+//  FROC3
+//
+//  Created by Anshul Ganumpally on 3/22/24.
+//
+
+import SwiftUI
+
+@main
+struct FROC3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
