14d15c267fed209a39cbf9aee738089764f6f43b
anshulg614 <122504590+anshulg614@users.noreply.github.com>
2024-07-04 23:32:01 -0400

    Initial Commit

---
+++ FROC5/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FROC5
+//
+//  Created by Anshul Ganumpally on 7/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FROC5/FROC5App.swift
@@ -0,0 +1,17 @@
+//
+//  FROC5App.swift
+//  FROC5
+//
+//  Created by Anshul Ganumpally on 7/4/24.
+//
+
+import SwiftUI
+
+@main
+struct FROC5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
